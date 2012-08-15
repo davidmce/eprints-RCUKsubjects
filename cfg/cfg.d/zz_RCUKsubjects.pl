03b8fa7e05e30a43639f6582d50dd46c15dcd881
David McElroy <David.McElroy@Glasgow.ac.uk>
2012-08-15 16:21:03 +0100

    updated cfg.d file with a zz to make it bloody work

---
+++ cfg/cfg.d/zz_RCUKsubjects.pl
@@ -0,0 +1,25 @@
+$c->{plugins}{"Screen::EPMC::RCUKsubjects"}{params}{disable} = 0;
+push @{$c->{browse_views}}, (
+	{	
+		id => "RCUKsubjects",
+		menus => [
+			{
+			fields => ["RCUKsubjects"],
+			hideempty => 1,
+			},
+			{
+			fields => [ "date;res=year" ],
+			reverse_order => 1,
+			allow_null => 1,
+			hideempty => 1,
+			},
+		],
+		order => "creators_name/title",
+		include => 1,
+		variations => [
+			"creators_name;first_letter",
+			"type",
+			"DEFAULT",
+		],
+	},
+);
